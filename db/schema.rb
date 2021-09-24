9e9e82d40f509dea0f9ae37df8d93e373ba89e15
Caio Lucas <caioprog18@gmail.com>
2021-09-24 11:01:45 -0300

    Limpando a aplicação

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_114012) do
-
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end

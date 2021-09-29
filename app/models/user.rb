981d2635507cc5fa6411ced73a12bb74f788dc9e
Caio Lucas <caioprog18@gmail.com>
2021-09-29 17:21:12 -0300

    Configurando a atenticação com devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :articles, dependent: :destroy
+
   # Include default devise modules. Others available are:
   #  :lockable, :timeoutable, :trackable and :omniauthable
   devise :confirmable,

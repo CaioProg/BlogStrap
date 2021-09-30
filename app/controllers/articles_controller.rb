73f9f6c3f26d5e67604c15e0c79737e59a7acf30
Caio Lucas <caioprog18@gmail.com>
2021-09-30 16:42:04 -0300

    Evita N+1 Queries

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,21 +4,23 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!, except: %i[index show]
   before_action :set_article, only: %i[show edit update destroy]
 
+
   def index
-    category = Category.find_by_name(params[:category]) if params[:category].present?
+    @categories = Category.sorted
+    category = @categories.select { |c| c.name == params[:category] }[0] if params[:category].present?
 
-    @highlights = Article.filter_by_category(category)
-                         .desc_order.
-                         first(3)
+    @highlights = Article.includes(:category, :user)
+                         .filter_by_category(category)
+                         .desc_order
+                         .first(3)
 
-    highlights_ids = @highlights.pluck(:id).join(',') 
+    highlight_ids = @highlights.pluck(:id).join(',') 
 
-    @articles = Article.without_highlights(highlights_ids)
+    @articles = Article.includes(:category, :user)
+                       .without_highlights(highlight_ids)
                        .filter_by_category(category)
                        .desc_order
                        .page(current_page)
-
-    @categories = Category.sorted
   end
   
   def show; end

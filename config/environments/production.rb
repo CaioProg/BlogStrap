59f9039e9edc0a57e9b80abe35a1499ccc75ba94
Caio Lucas <caioprog18@gmail.com>
2021-11-12 11:00:10 -0300

    Deploy no Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,7 +119,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # Email
-  config.action_mailer.default_url_options = { host: 'blogstrap-ruby.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'blogstraprb.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
